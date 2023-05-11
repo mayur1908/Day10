eb3d1c896d7be23453f5db5ab905e2c763da3472
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-11 11:30:09 +0530

    [added program.cs file]

---
+++ EmpWageBuilderArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpWageBuilderArray
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilderArray = new EmpWageBuilderArray();
+            empWageBuilderArray.addCompanyEmpWage("DMart", 20, 2, 10);
+            empWageBuilderArray.addCompanyEmpWage("Reliance", 20, 50, 60);
+
+            empWageBuilderArray.computeEmpWage();
+
+        }
+    }
+}
